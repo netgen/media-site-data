f6beb630b14ab2f5afed6a1fe516472a5d311a92
Edi Modrić <edi.modric@gmail.com>
2018-08-18 22:17:09 +0200

    Add Netgen Layouts 0.12 tables to schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -2494,6 +2494,313 @@ CREATE TABLE `ezworkflow_process` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `ngbm_block`
+--
+
+DROP TABLE IF EXISTS `ngbm_block`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_block` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `layout_id` int(11) NOT NULL,
+  `depth` int(11) NOT NULL,
+  `path` varchar(191) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `placeholder` varchar(191) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `definition_identifier` varchar(191) NOT NULL,
+  `view_type` varchar(191) NOT NULL,
+  `item_view_type` varchar(191) NOT NULL,
+  `name` varchar(191) NOT NULL,
+  `config` text NOT NULL,
+  `translatable` tinyint(4) NOT NULL,
+  `main_locale` varchar(191) NOT NULL,
+  `always_available` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_layout` (`layout_id`,`status`),
+  KEY `idx_ngl_parent_block` (`parent_id`,`placeholder`,`status`),
+  CONSTRAINT `fk_ngl_block_layout` FOREIGN KEY (`layout_id`, `status`) REFERENCES `ngbm_layout` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_block_collection`
+--
+
+DROP TABLE IF EXISTS `ngbm_block_collection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_block_collection` (
+  `block_id` int(11) NOT NULL,
+  `block_status` int(11) NOT NULL,
+  `identifier` varchar(191) NOT NULL,
+  `collection_id` int(11) NOT NULL,
+  `collection_status` int(11) NOT NULL,
+  PRIMARY KEY (`block_id`,`block_status`,`identifier`),
+  KEY `idx_ngl_block` (`block_id`,`block_status`),
+  KEY `idx_ngl_collection` (`collection_id`,`collection_status`),
+  CONSTRAINT `fk_ngl_block_collection_block` FOREIGN KEY (`block_id`, `block_status`) REFERENCES `ngbm_block` (`id`, `status`),
+  CONSTRAINT `fk_ngl_block_collection_collection` FOREIGN KEY (`collection_id`, `collection_status`) REFERENCES `ngbm_collection` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_block_translation`
+--
+
+DROP TABLE IF EXISTS `ngbm_block_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_block_translation` (
+  `block_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `locale` varchar(191) NOT NULL,
+  `parameters` text NOT NULL,
+  PRIMARY KEY (`block_id`,`status`,`locale`),
+  CONSTRAINT `fk_ngl_block_translation_block` FOREIGN KEY (`block_id`, `status`) REFERENCES `ngbm_block` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_collection`
+--
+
+DROP TABLE IF EXISTS `ngbm_collection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_collection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `start` int(11) NOT NULL,
+  `length` int(11) DEFAULT NULL,
+  `translatable` tinyint(4) NOT NULL,
+  `main_locale` varchar(191) NOT NULL,
+  `always_available` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`,`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_collection_item`
+--
+
+DROP TABLE IF EXISTS `ngbm_collection_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_collection_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `collection_id` int(11) NOT NULL,
+  `position` int(11) NOT NULL,
+  `value` varchar(191) DEFAULT NULL,
+  `value_type` varchar(191) NOT NULL,
+  `config` text NOT NULL,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_collection` (`collection_id`,`status`),
+  CONSTRAINT `fk_ngl_item_collection` FOREIGN KEY (`collection_id`, `status`) REFERENCES `ngbm_collection` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_collection_query`
+--
+
+DROP TABLE IF EXISTS `ngbm_collection_query`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_collection_query` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `collection_id` int(11) NOT NULL,
+  `type` varchar(191) NOT NULL,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_collection` (`collection_id`,`status`),
+  CONSTRAINT `fk_ngl_query_collection` FOREIGN KEY (`collection_id`, `status`) REFERENCES `ngbm_collection` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_collection_query_translation`
+--
+
+DROP TABLE IF EXISTS `ngbm_collection_query_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_collection_query_translation` (
+  `query_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `locale` varchar(191) NOT NULL,
+  `parameters` text NOT NULL,
+  PRIMARY KEY (`query_id`,`status`,`locale`),
+  CONSTRAINT `fk_ngl_query_translation_query` FOREIGN KEY (`query_id`, `status`) REFERENCES `ngbm_collection_query` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_collection_translation`
+--
+
+DROP TABLE IF EXISTS `ngbm_collection_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_collection_translation` (
+  `collection_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `locale` varchar(191) NOT NULL,
+  PRIMARY KEY (`collection_id`,`status`,`locale`),
+  CONSTRAINT `fk_ngl_collection_translation_collection` FOREIGN KEY (`collection_id`, `status`) REFERENCES `ngbm_collection` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_layout`
+--
+
+DROP TABLE IF EXISTS `ngbm_layout`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_layout` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `type` varchar(191) NOT NULL,
+  `name` varchar(191) NOT NULL,
+  `description` text NOT NULL,
+  `created` int(11) NOT NULL,
+  `modified` int(11) NOT NULL,
+  `shared` tinyint(1) NOT NULL,
+  `main_locale` varchar(191) NOT NULL,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_layout_name` (`name`),
+  KEY `idx_ngl_layout_type` (`type`),
+  KEY `idx_ngl_layout_shared` (`shared`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_layout_translation`
+--
+
+DROP TABLE IF EXISTS `ngbm_layout_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_layout_translation` (
+  `layout_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `locale` varchar(191) NOT NULL,
+  PRIMARY KEY (`layout_id`,`status`,`locale`),
+  CONSTRAINT `fk_ngl_layout_translation_layout` FOREIGN KEY (`layout_id`, `status`) REFERENCES `ngbm_layout` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_migration_versions`
+--
+
+DROP TABLE IF EXISTS `ngbm_migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_rule`
+--
+
+DROP TABLE IF EXISTS `ngbm_rule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_rule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `layout_id` int(11) DEFAULT NULL,
+  `comment` text NOT NULL,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_related_layout` (`layout_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_rule_condition`
+--
+
+DROP TABLE IF EXISTS `ngbm_rule_condition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_rule_condition` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `rule_id` int(11) NOT NULL,
+  `type` varchar(191) NOT NULL,
+  `value` text,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_rule` (`rule_id`,`status`),
+  CONSTRAINT `fk_ngl_condition_rule` FOREIGN KEY (`rule_id`, `status`) REFERENCES `ngbm_rule` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_rule_data`
+--
+
+DROP TABLE IF EXISTS `ngbm_rule_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_rule_data` (
+  `rule_id` int(11) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `priority` int(11) NOT NULL,
+  PRIMARY KEY (`rule_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_rule_target`
+--
+
+DROP TABLE IF EXISTS `ngbm_rule_target`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_rule_target` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) NOT NULL,
+  `rule_id` int(11) NOT NULL,
+  `type` varchar(191) NOT NULL,
+  `value` text,
+  PRIMARY KEY (`id`,`status`),
+  KEY `idx_ngl_rule` (`rule_id`,`status`),
+  KEY `idx_ngl_target_type` (`type`),
+  CONSTRAINT `fk_ngl_target_rule` FOREIGN KEY (`rule_id`, `status`) REFERENCES `ngbm_rule` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ngbm_zone`
+--
+
+DROP TABLE IF EXISTS `ngbm_zone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ngbm_zone` (
+  `identifier` varchar(191) NOT NULL,
+  `layout_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `root_block_id` int(11) NOT NULL,
+  `linked_layout_id` int(11) DEFAULT NULL,
+  `linked_zone_identifier` varchar(191) DEFAULT NULL,
+  PRIMARY KEY (`identifier`,`layout_id`,`status`),
+  KEY `idx_ngl_layout` (`layout_id`,`status`),
+  KEY `idx_ngl_root_block` (`root_block_id`,`status`),
+  KEY `idx_ngl_linked_zone` (`linked_layout_id`,`linked_zone_identifier`),
+  CONSTRAINT `fk_ngl_zone_block` FOREIGN KEY (`root_block_id`, `status`) REFERENCES `ngbm_block` (`id`, `status`),
+  CONSTRAINT `fk_ngl_zone_layout` FOREIGN KEY (`layout_id`, `status`) REFERENCES `ngbm_layout` (`id`, `status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `nguser_setting`
 --
 
