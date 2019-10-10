a605508b30dda2ab6a1185fa5f91883638aad9bc
Edi Modrić <edi.modric@gmail.com>
2019-10-10 10:49:45 +0200

    Update eZ version in ezsite_data tables

--- netgen-media/data.sql
+++ netgen-media/data.sql
@@ -4229,7 +4229,7 @@ UNLOCK TABLES;
 LOCK TABLES `ezsite_data` WRITE;
 /*!40000 ALTER TABLE `ezsite_data` DISABLE KEYS */;
 INSERT INTO `ezsite_data` VALUES ('ezpublish-release','1');
-INSERT INTO `ezsite_data` VALUES ('ezpublish-version','7.5.3');
+INSERT INTO `ezsite_data` VALUES ('ezpublish-version','8.0.0');
 /*!40000 ALTER TABLE `ezsite_data` ENABLE KEYS */;
 UNLOCK TABLES;
 
