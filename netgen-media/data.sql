f5f381cd83e579a38f22c90c96a4ac1b98df84bb
Edi Modrić <edi.modric@gmail.com>
2020-04-17 17:26:29 +0200

    Update schema for eZ kernel 7.5.7

--- netgen-media/data.sql
+++ netgen-media/data.sql
@@ -5878,7 +5878,7 @@ UNLOCK TABLES;
 LOCK TABLES `ezsite_data` WRITE;
 /*!40000 ALTER TABLE `ezsite_data` DISABLE KEYS */;
 INSERT INTO `ezsite_data` VALUES ('ezpublish-release','1');
-INSERT INTO `ezsite_data` VALUES ('ezpublish-version','7.5.3');
+INSERT INTO `ezsite_data` VALUES ('ezpublish-version','7.5.7');
 /*!40000 ALTER TABLE `ezsite_data` ENABLE KEYS */;
 UNLOCK TABLES;
 
